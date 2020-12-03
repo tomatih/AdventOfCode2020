dbd61aebbc8ca2ebbd572ff15a710302965adba8
tomatih <maatteeoh@wp.pl>
2020-12-03 11:33:34 +0000

    Day 3 solution

---
+++ Day3/silver.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+pos = 0
+out = 0
+with open("input.txt") as f:
+	for row in f.readlines():
+		row = row.strip()
+		if row[pos%len(row)] == "#":
+			out += 1
+		pos+=3
+
+print(out)
